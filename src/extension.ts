4c42929cc07f75a4924345e84441a82459525a23
Kai Chang <alfred.kai.chang@gmail.com>
2021-03-05 01:05:19 +0800

    [init]

---
+++ src/extension.ts
@@ -0,0 +1,46 @@
+import * as http from 'http';
+import * as vscode from 'vscode';
+
+const CEO_SERVER = 'http://ceospeech.ddns.net/';
+const CEO_INTERVAL = 1500000;
+
+export function activate(context: vscode.ExtensionContext) {
+	let first = true;
+
+	const interval = setInterval(async () => {
+		try {
+			const msg = (await request(CEO_SERVER)) as string;
+			if (first) {
+				vscode.window.showInformationMessage('您的CEO已經上線！');
+				vscode.window.showInformationMessage(msg);
+				first = false;
+			} else {
+				vscode.window.showInformationMessage(msg);
+			}
+		} catch (err) {
+			console.log(err);
+			vscode.window.showErrorMessage('CEO現在忙碌中！');
+			clearInterval(interval);
+		}
+	}, CEO_INTERVAL);
+}
+
+function request(url: string) {
+	return new Promise((resolve, reject) => {
+		http
+			.get(url, (resp) => {
+				let data = '';
+
+				resp.on('data', (chunk) => {
+					data += chunk;
+				});
+
+				resp.on('end', () => {
+					resolve(data);
+				});
+			})
+			.on('error', (err) => {
+				reject(err);
+			});
+	});
+}
