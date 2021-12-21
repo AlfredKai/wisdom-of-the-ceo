74512307e069c64546035d288768a7471a77a5c8
Kai Chang <alfred.kai.chang@gmail.com>
2021-12-22 00:00:53 +0800

    [feat] update to offline ver.

--- src/extension.ts
+++ src/extension.ts
@@ -1,53 +1,17 @@
-import * as http from 'http';
 import * as vscode from 'vscode';
 
-const CEO_SERVER = 'http://ceospeech.ddns.net/';
+import { WISDOMS } from './wisdoms';
+
 const CEO_INTERVAL = 1500000;
 
 export function activate(context: vscode.ExtensionContext) {
-  (async () => {
-    try {
-      await callCeo(true);
-      const interval = setInterval(async () => {
-        try {
-          callCeo(false);
-        } catch (err) {
-          vscode.window.showErrorMessage('CEO現在忙碌中！');
-          clearInterval(interval);
-        }
-      }, CEO_INTERVAL);
-    } catch (err) {
-      vscode.window.showErrorMessage('CEO現在忙碌中！');
-    }
-  })();
+  setInterval(() => {
+    vscode.window.showErrorMessage(WISDOMS[getRandomInt(0, WISDOMS.length - 1)]);
+  }, CEO_INTERVAL);
 }
 
-async function callCeo(first: boolean) {
-  const msg = (await request(CEO_SERVER)) as string;
-  if (first) {
-    vscode.window.showInformationMessage('您的CEO已經上線！');
-    vscode.window.showInformationMessage(msg);
-  } else {
-    vscode.window.showInformationMessage(msg);
-  }
-}
-
-function request(url: string) {
-  return new Promise((resolve, reject) => {
-    http
-      .get(url, (resp) => {
-        let data = '';
-
-        resp.on('data', (chunk) => {
-          data += chunk;
-        });
-
-        resp.on('end', () => {
-          resolve(data);
-        });
-      })
-      .on('error', (err) => {
-        reject(err);
-      });
-  });
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
